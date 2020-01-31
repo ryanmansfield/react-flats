743b0b12bdc8588c9f0b026e64d2f39f6a101338
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-01-31 14:10:57 -0600

    Created Flat and Flat List components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import FlatList from './flat_list.jsx';
+import Flat from './flat.jsx';
+
 class App extends Component {
   render () {
     return (
       <div>
-        <div className="flat-list">flat list</div>
-        <div className="map-container">map </div>
+        <div className="flat-list">
+          <FlatList />
+        </div>
+        <div className="map-container">
+          map
+        </div>
       </div>
     );
   }
---
+++ src/components/flat_list.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import flat from './flat.jsx';
+
+class FlatList extends Component {
+  render () {
+    return (
+      <h1>flatlist</h1>
+    );
+  }
+}
+
+export default FlatList;
