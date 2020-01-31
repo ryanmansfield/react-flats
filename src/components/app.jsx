af549e47aeb3284a522cc1c2ca6223e9ea98db85
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-01-31 13:47:49 -0600

    App Component created and styling rules added

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div className="flat-list">flat list</div>
+        <div className="map-container">map </div>
+      </div>
+    );
+  }
+}
+
+export default App;
